c6cb766123718a3922da9da0d45695d069aa4f88
Chris <chris.hu@live.nl>
2021-01-20 19:20:52 +0100

    Deleted unused file

--- app/src/main/java/com/example/madfinalapp/model/TriviaIncorrectAnswerList.kt
+++
@@ -1,8 +0,0 @@
-package com.example.madfinalapp.model
-
-import com.google.gson.annotations.SerializedName
-
-data class TriviaIncorrectAnswerList(
-    @SerializedName("incorrect_answers")
-    var incorrectAnswer: List<String>
-)
