372a79b543ebdb19bfdd9bdc4ae092f6e3a51c72
Chris <chris.hu@hva.nl>
2021-01-24 15:13:39 +0100

    Delete trivia record on the history page

--- app/src/main/java/com/example/madfinalapp/ui/TriviaHistoryFragment.kt
+++ app/src/main/java/com/example/madfinalapp/ui/TriviaHistoryFragment.kt
@@ -1,13 +1,13 @@
 package com.example.madfinalapp.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madfinalapp.R
@@ -57,11 +57,38 @@ class TriviaHistoryFragment : Fragment() {
             )
         )
 
-//        createItemTouchHelper().attachToRecyclerView(rvReminders)
+        createItemTouchHelper().attachToRecyclerView(rvTriviaHistory)
     }
 
     private fun onMovieClick(index: Int) {
         Toast.makeText(context, "Hello World!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val triviaRecordToDelete = triviaCategory[position]
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    withContext(Dispatchers.IO) {
+                        triviaRecordRepository.deleteTriviaRecord(triviaRecordToDelete)
+                    }
+                    getTriviaRecordsFromDatabase()
+                }
+            }
+        }
+        return ItemTouchHelper(callback)
     }
 
     private fun getTriviaRecordsFromDatabase() {
