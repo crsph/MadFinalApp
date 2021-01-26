79d19c2f81c3eb091601271e647b8522d98e658e
Chris <chris.hu@live.nl>
2021-01-26 15:46:38 +0100

    Deleted color observer

--- app/src/main/java/com/example/madfinalapp/ui/TriviaCategoryFragment.kt
+++ app/src/main/java/com/example/madfinalapp/ui/TriviaCategoryFragment.kt
@@ -5,34 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.madfinalapp.R
-import com.example.madfinalapp.model.Trivia
-import com.example.madfinalapp.repository.TriviaRecordRepository
-import com.example.madfinalapp.repository.TriviaRepository
 import com.example.madfinalapp.vm.TriviaViewModel
 import kotlinx.android.synthetic.main.fragment_trivia_category.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.*
 import kotlin.concurrent.schedule
 
 
-//const val REQ_COLOR_KEY = "req_color"
-//const val BUNDLE_COLOR_KEY = "bundle_color"
-
 class TriviaCategoryFragment : Fragment() {
 
     private val viewModel: TriviaViewModel by activityViewModels()
-    private val triviaList = arrayListOf<Trivia>()
-    private lateinit var triviaRecordRepository: TriviaRecordRepository
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,40 +28,25 @@ class TriviaCategoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        triviaRecordRepository = TriviaRecordRepository(requireContext())
-//
-//        CoroutineScope(Dispatchers.Main).launch {
-//            withContext(Dispatchers.IO) {
-//                triviaRecordRepository.deleteAll()
-//            }
-//        }
-
         loadingUIToggler(false)
 
-//        observeTrivia()
-
         btnGeneral.setOnClickListener {
-//            changeAnswerButtonColor(0)
             navigateToQuestionnaire(9)
         }
 
         btnBooks.setOnClickListener {
-//            changeAnswerButtonColor(1)
             navigateToQuestionnaire(10)
         }
 
         btnFilms.setOnClickListener {
-//            changeAnswerButtonColor(2)
             navigateToQuestionnaire(11)
         }
 
         btnMusic.setOnClickListener {
-//            changeAnswerButtonColor(3)
             navigateToQuestionnaire(12)
         }
 
         btnMusicals.setOnClickListener {
-//            changeAnswerButtonColor(4)
             navigateToQuestionnaire(13)
         }
 
@@ -103,26 +73,8 @@ class TriviaCategoryFragment : Fragment() {
         }
     }
 
-
-    private fun observeTrivia() {
-        viewModel.trivia.observe(viewLifecycleOwner, Observer {
-            triviaList.clear()
-            triviaList.addAll(it)
-        })
-
-        viewModel.errorText.observe(viewLifecycleOwner, Observer {
-            Toast.makeText(activity, it, Toast.LENGTH_SHORT).show()
-        })
-    }
-
     private fun loadingUIToggler(boolean: Boolean) {
         ivOverlay.isVisible = boolean
         loadingPanel.isVisible = boolean
     }
-
-//    private fun changeAnswerButtonColor(colorIndex: Int) {
-//        setFragmentResult(REQ_COLOR_KEY, bundleOf(Pair(BUNDLE_COLOR_KEY, colorIndex)))
-//
-//            findNavController().popBackStack()
-//    }
 }
